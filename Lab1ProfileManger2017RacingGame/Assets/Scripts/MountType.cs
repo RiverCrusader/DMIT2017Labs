f3970ca9df34b1e19a0618dcabf9ccfc214f6523
RiverCrusader <jturcotte5@nait.ca>
2026-01-14 00:49:38 -0500

    brough in a title screen I built last seem, and create outline for mount type

---
+++ Lab1ProfileManger2017RacingGame/Assets/Scripts/MountType.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MountType : MonoBehaviour
+{
+    string mountName;
+    int mountSpeed;
+    int acceloration;
+    int offRoadSpeed;
+    bool canJump;
+
+    public MountType(string name_, int speed_, int offRoadSpeed_, int acceloration_, bool canJump_)
+    {
+
+        mountName = name_;
+        mountSpeed = speed_;
+        offRoadSpeed = offRoadSpeed_;
+        acceloration = acceloration_;
+        canJump = canJump_;
+
+    }
+
+}
--- Lab1ProfileManger2017RacingGame/Assets/Scripts/Obstacles.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Obstacles : MonoBehaviour
-{
-    public int slowAmount;
-}
