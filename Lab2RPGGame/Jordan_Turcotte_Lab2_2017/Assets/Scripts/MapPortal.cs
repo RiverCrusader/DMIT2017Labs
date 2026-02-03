82c936efbd2b33c2c44bcaff691858d7b10335f0
RiverCrusader <jturcotte5@nait.ca>
2026-02-03 16:12:39 -0500

    added more map conrtrol so i can change the cell sizedbased on the tilemap, so I can use different tileseets

--- Lab2RPGGame/Jordan_Turcotte_Lab2_2017/Assets/Scripts/MapPortal.cs
+++ Lab2RPGGame/Jordan_Turcotte_Lab2_2017/Assets/Scripts/MapPortal.cs
@@ -1,14 +1,20 @@
+using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class MapPortal : MonoBehaviour
 {
     public int targetMap;
     public int targetEntryPoint;
+    public List<Vector3> cellSize = new List<Vector3>();
+    public int cellSizeType;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag != "Player") return;
 
         MapNavigation.Instance.GoToMap(targetMap, targetEntryPoint);
+
+        //make a switch for cell sized that takes cell sixe type
     }
 }
