bbbb11e87f08b96f843fdf7085d5757e7ae58116
RiverCrusader <jturcotte5@nait.ca>
2026-02-27 12:49:15 -0500

    added new game and load game functionality

--- Lab2RPGGame/Jordan_Turcotte_Lab2_2017/Assets/Scripts/SaveAndLoad/TittleScreenUIControl.cs
+++ Lab2RPGGame/Jordan_Turcotte_Lab2_2017/Assets/Scripts/SaveAndLoad/TittleScreenUIControl.cs
@@ -1,15 +1,17 @@
+using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TittleScreenUIControl : MonoBehaviour
 {
     public void NewGame()
     {
-        
+        File.Delete("Assets/Resources/save.json");
         LoadGame();
     }
     public void LoadGame()
     {
-        
+        SceneManager.LoadScene("RPG_World");
     }
     public void Exit()
     {
